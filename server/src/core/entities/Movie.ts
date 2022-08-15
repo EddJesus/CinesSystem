9a0251782a86cf3790628359b3bb3edc219f5436
EddJesus <edusanto22@gmail.com>
2022-08-15 02:39:41 -0300

    feat: add setters to Movie Entity

--- server/src/core/entities/Movie.ts
+++ server/src/core/entities/Movie.ts
@@ -13,4 +13,28 @@ export class Movie extends Entity<MovieProps> {
     constructor(props: MovieProps, id?: string) {
         super(props, id)
     }
+
+    set name(name: string) {
+        this.props.name = name
+    }
+
+    set gender(gender: string){
+        this.props.gender = gender
+    }
+
+    set durationInMinutes(durationInMinutes: number){
+        this.props.durationInMinutes = durationInMinutes
+    }
+
+    set classification(classification: number){
+        this.props.classification = classification
+    }
+
+    set synopsis(synopsis: string){
+        this.props.synopsis = synopsis
+    }
+
+    set releaseDate(releaseDate: Date){
+        this.props.releaseDate = releaseDate
+    }
 }
