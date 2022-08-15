c063ac8552b26feb47cdad6726352bad2749d891
EddJesus <edusanto22@gmail.com>
2022-08-15 01:45:42 -0300

    test: GetMovie useCase

---
+++ server/src/core/usecases/movies/getMovie/get-movie.usecase.spec.ts
@@ -0,0 +1,38 @@
+import { MovieRepositoryInMemory } from "../../../../infra/repositories/movie.repository-in-memory";
+import { Movie } from "../../../entities/Movie";
+import { GetMovieUseCase } from "./get-movie.usecase";
+
+describe('getMovieUseCase', () => {
+  it('should be defined', () => {
+    const movieRepositoryInMemory = new MovieRepositoryInMemory();
+    const sut = new GetMovieUseCase(movieRepositoryInMemory)
+
+    expect(sut).toBeDefined();
+  })
+
+  it('should get a movie', async () => {
+    const movie: Movie = new Movie({
+      name: 'Movie 1',
+      gender: 'horror',
+      durationInMinutes: 120,
+      synopsis: 'Movie Synopsis',
+      releaseDate: new Date(),
+      classification: 18,
+    })
+    const movieRepositoryInMemory = new MovieRepositoryInMemory();
+    await movieRepositoryInMemory.create(movie)
+    const sut = new GetMovieUseCase(movieRepositoryInMemory)
+
+    const mockedMovie = await sut.execute(movie.id)
+
+    expect(mockedMovie).toBeInstanceOf(Movie);
+  })
+
+  it('should throw an error if movie not found', async () => {
+    const invalidId = 'invalid-id'
+    const movieRepositoryInMemory = new MovieRepositoryInMemory();
+    const sut = new GetMovieUseCase(movieRepositoryInMemory)
+
+    await expect(sut.execute(invalidId)).rejects.toThrowError(`Movie with id ${invalidId} not found`)
+  })
+})
