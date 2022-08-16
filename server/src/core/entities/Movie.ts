191f727c40e1b9d105a4ba13b5808879db63ed30
EddJesus <edusanto22@gmail.com>
2022-08-16 14:18:56 -0300

    feat: add getters to Movie Entity

--- server/src/core/entities/Movie.ts
+++ server/src/core/entities/Movie.ts
@@ -14,26 +14,50 @@ export class Movie extends Entity<MovieProps> {
         super(props, id)
     }
 
+    get name(): string {
+        return this.props.name
+    }
+
     set name(name: string) {
         this.props.name = name
+    }
+
+    get gender(): string {
+        return this.props.gender
     }
 
     set gender(gender: string){
         this.props.gender = gender
     }
 
+    get durationInMinutes(): number {
+        return this.props.durationInMinutes
+    }
+
     set durationInMinutes(durationInMinutes: number){
         this.props.durationInMinutes = durationInMinutes
+    }
+
+    get classification(): number {
+        return this.props.classification
     }
 
     set classification(classification: number){
         this.props.classification = classification
     }
 
+    get synopsis(): string {
+        return this.props.synopsis
+    }
+
     set synopsis(synopsis: string){
         this.props.synopsis = synopsis
     }
 
+    get releaseDate(): Date {
+        return this.props.releaseDate
+    }
+    
     set releaseDate(releaseDate: Date){
         this.props.releaseDate = releaseDate
     }
