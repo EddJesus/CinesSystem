8bb950c07269f7cd9700f7c882c0d341ad687ccc
EddJesus <edusanto22@gmail.com>
2022-08-22 19:56:41 -0300

    feat: add getters and setters to Session Entity

--- server/src/core/entities/Session.ts
+++ server/src/core/entities/Session.ts
@@ -12,4 +12,28 @@ export class Session extends Entity<SessionProps> {
     constructor(props: SessionProps, id?: string) {
         super(props, id)
     }
+
+    get date(): Date {
+        return this.props.date
+    }
+
+    get movie(): Movie {
+        return this.props.movie
+    }
+
+    get cine(): Cine {
+        return this.props.cine
+    }
+
+    set date(date: Date) {
+        this.props.date = date
+    }
+
+    set movie(movie: Movie) {
+        this.props.movie = movie
+    }
+
+    set cine(cine: Cine) {
+        this.props.cine = cine
+    }
 }
