50a4b9cd0e429cd6444fce99bf5f6b0733e5fca5
EddJesus <edusanto22@gmail.com>
2022-08-22 20:00:14 -0300

    test: ListSessions usecase

---
+++ server/src/core/usecases/sessions/listSessions/list-sessions.usecase.spec.ts
@@ -0,0 +1,28 @@
+import { SessionRepositoryInMemory } from "../../../../infra/repositories/session.repository-in-memory";
+import { Session } from "../../../entities/Session";
+import { makeMockSession } from "../__mocks__/Session";
+import { ListSessionsUseCase } from "./list-sessions.usecase";
+
+describe('ListSessionsUsecase', () => {
+  it('should be defined', () => {
+    const sessionRepositoryInMemory = new SessionRepositoryInMemory();
+    const sut = new ListSessionsUseCase(sessionRepositoryInMemory)
+
+    expect(sut).toBeDefined();
+  })
+
+  it('should list all sessions', async () => {
+    const sessionRepositoryInMemory = new SessionRepositoryInMemory();
+    const sut = new ListSessionsUseCase(sessionRepositoryInMemory)
+
+    const session1: Session = makeMockSession()
+    const session2: Session = makeMockSession()
+
+    await sessionRepositoryInMemory.create(session1)
+    await sessionRepositoryInMemory.create(session2)
+
+    const sessions = await sut.execute()
+    
+    expect(sessions).toHaveLength(2)
+  })
+})
