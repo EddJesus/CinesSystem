1022f7ebb018caef6ff2a0b47cd715cb88433330
EddJesus <edusanto22@gmail.com>
2022-08-22 14:56:02 -0300

    feat: implements cine repository interface

---
+++ server/src/infra/repositories/cine.repository-in-memory.ts
@@ -0,0 +1,40 @@
+import { Cine } from "../../core/entities/Cine";
+import { CineRepository } from "../../core/repositories/cine.repository";
+
+export class CineRepositoryInMemory implements CineRepository {
+    private cines: Cine[] = [];
+    async create(cine: Cine): Promise<Cine> {
+        this.cines.push(cine);
+        return cine;
+    }
+
+    async getById(id: string): Promise<Cine> {
+        const cine = this.cines.find(cine => cine.id === id);
+        if (!cine) {
+            throw new Error(`Cine with id ${id} not found`);
+        }
+        return cine;
+    }
+
+    async list(): Promise<Cine[]> {
+        return this.cines;
+    }
+
+    async update(cine: Cine): Promise<Cine> {
+        const index = this.cines.findIndex(cine => cine.id === cine.id);
+        if (index === -1) {
+            throw new Error(`Cine with id ${cine.id} not found`);
+        }
+        this.cines[index] = cine;
+        return cine;
+    }
+    
+    async delete(id: string): Promise<boolean> {
+        const index = this.cines.findIndex(cine => cine.id === id);
+        if (index === -1) {
+            throw new Error(`Cine with id ${id} not found`);
+        }
+        this.cines.splice(index, 1);
+        return true
+    }
+}
