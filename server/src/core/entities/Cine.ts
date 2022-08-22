ca31f5ee1fb5c98916e048eb55f227fe38b792b3
EddJesus <edusanto22@gmail.com>
2022-08-22 14:54:50 -0300

    refactor: complete Cine entity

--- server/src/core/entities/Cine.ts
+++ server/src/core/entities/Cine.ts
@@ -10,4 +10,28 @@ export class Cine extends Entity<CineProps> {
     constructor(props: CineProps, id?: string) {
         super(props, id)
     }
+
+    get name(): string {
+        return this.props.name
+    }
+
+    get city(): string {
+        return this.props.city
+    }
+
+    get state(): string {
+        return this.props.state
+    }
+
+    set name(name: string) {
+        this.props.name = name
+    }
+
+    set city(city: string) {
+        this.props.city = city
+    }
+
+    set state(state: string) {
+        this.props.state = state
+    }
 }
